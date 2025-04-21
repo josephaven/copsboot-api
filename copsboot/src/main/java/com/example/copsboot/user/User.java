35bf3c3127db4c1f270d38474999dfe2da0d77a8
josephaven <jr457944@gmail.com>
2025-04-21 02:57:46 -0600

    se agregó la entidad User con roles y UUID como ID

---
+++ copsboot/src/main/java/com/example/copsboot/user/User.java
@@ -0,0 +1,50 @@
+package com.example.copsboot.user;
+
+import com.example.orm.jpa.AbstractEntity;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "copsboot_user")
+public class User extends AbstractEntity<UserId> {
+
+    private String email;
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Set<UserRole> roles;
+
+    protected User() {
+
+    }
+
+    public User(UserId id, String email, String password, Set<UserRole> roles) {
+        super(id);
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+}
