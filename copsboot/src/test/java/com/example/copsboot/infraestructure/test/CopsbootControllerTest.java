8c43d92d9596992b098137d19c9987370d817fc3
josephaven <jr457944@gmail.com>
2025-04-26 03:09:27 -0600

    se agregaron pruebas REST para el controlador de usuario con configuración personalizada, documentación generada y validación de respuestas usando MockMvc

---
+++ copsboot/src/test/java/com/example/copsboot/infraestructure/test/CopsbootControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.copsboot.infraestructure.test;
+
+import com.c4_soft.springaddons.security.oauth2.test.webmvc.AutoConfigureAddonsWebmvcResourceServerSecurity;
+import com.example.copsboot.infrastructure.security.WebSecurityConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Retention(RetentionPolicy.RUNTIME) //<.>
+@WebMvcTest //<.>
+@AutoConfigureAddonsWebmvcResourceServerSecurity //<.>
+@Import(WebSecurityConfiguration.class) //<.>
+public @interface CopsbootControllerTest {
+
+    @AliasFor(annotation = WebMvcTest.class, attribute = "value") //<5>
+    Class<?>[] value() default {};
+
+    @AliasFor(annotation = WebMvcTest.class, attribute = "controllers") //<6>
+    Class<?>[] controllers() default {};
+}
+
