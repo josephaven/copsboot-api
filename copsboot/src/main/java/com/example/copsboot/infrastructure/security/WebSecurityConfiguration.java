68683471232f3ed9d492bd2d02d7ab91dc134223
josephaven <jr457944@gmail.com>
2025-04-26 02:00:59 -0600

    se actualizo la configuración de seguridad para usar ResourceServerExpressionInterceptUrlRegistry con SpringAddons

--- copsboot/src/main/java/com/example/copsboot/infrastructure/security/WebSecurityConfiguration.java
+++ copsboot/src/main/java/com/example/copsboot/infrastructure/security/WebSecurityConfiguration.java
@@ -1,24 +1,19 @@
 package com.example.copsboot.infrastructure.security;
 
+import com.c4_soft.springaddons.security.oidc.starter.synchronised.resourceserver.ResourceServerExpressionInterceptUrlRegistryPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
+@EnableMethodSecurity //<.>
 public class WebSecurityConfiguration {
+
     @Bean
-    SecurityFilterChain configureSecurityFilterChain(HttpSecurity http) throws Exception {
-
-        http
-                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll() //<.>
-                        .requestMatchers("/api/**").authenticated() //<.>
-                        .anyRequest().authenticated())
-                .oauth2ResourceServer(it -> it.jwt(Customizer.withDefaults())); //<.>
-
-        return http.build();
+    ResourceServerExpressionInterceptUrlRegistryPostProcessor authorizePostProcessor() { //<.>
+        return registry -> registry.requestMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().authenticated();
     }
 }
