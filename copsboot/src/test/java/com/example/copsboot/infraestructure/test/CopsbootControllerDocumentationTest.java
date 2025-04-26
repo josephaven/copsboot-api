456c5715b4405d03022f6eda5bec81b464265422
josephaven <jr457944@gmail.com>
2025-04-26 03:09:07 -0600

    se agregaron pruebas REST para el controlador de usuario con configuración personalizada, documentación generada y validación de respuestas usando MockMvc

---
+++ copsboot/src/test/java/com/example/copsboot/infraestructure/test/CopsbootControllerDocumentationTest.java
@@ -0,0 +1,29 @@
+package com.example.copsboot.infraestructure.test;
+
+import com.c4_soft.springaddons.security.oauth2.test.webmvc.AutoConfigureAddonsWebmvcResourceServerSecurity;
+import com.example.copsboot.infrastructure.security.WebSecurityConfiguration;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@CopsbootControllerTest
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
+@ContextConfiguration(classes = CopsbootControllerDocumentationTestConfiguration.class)
+public @interface CopsbootControllerDocumentationTest {
+
+    @AliasFor(annotation = WebMvcTest.class, attribute = "value") //<5>
+    Class<?>[] value() default {};
+
+    @AliasFor(annotation = WebMvcTest.class, attribute = "controllers") //<6>
+    Class<?>[] controllers() default {};
+}
