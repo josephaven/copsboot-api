91be5a9e0b40d813d091c3d325d5f6e7603f341c
josephaven <jr457944@gmail.com>
2025-04-26 22:13:34 -0600

    se agregó validador CreateReportRequestValidator para validar número de autos en incidentes de tráfico

---
+++ copsboot/src/main/java/com/example/copsboot/report/web/CreateReportRequestValidator.java
@@ -0,0 +1,20 @@
+package com.example.copsboot.report.web;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CreateReportRequestValidator implements ConstraintValidator<ValidCreateReportRequest, CreateReportRequest> { //<1>
+
+    @Override
+    public void initialize(ValidCreateReportRequest constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(CreateReportRequest value, ConstraintValidatorContext context) {
+        boolean result = true;
+        if (value.trafficIncident() && value.numberOfInvolvedCars() <= 0) { //<2>
+            result = false;
+        }
+        return result;
+    }
+}
