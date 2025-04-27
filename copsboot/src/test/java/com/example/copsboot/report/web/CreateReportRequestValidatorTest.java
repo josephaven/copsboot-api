67f03510a8c7cfd4d2b106fbb148380977a571c6
josephaven <jr457944@gmail.com>
2025-04-26 22:14:20 -0600

    se agregó test CreateReportRequestValidatorTest para validar reglas de incidentes de tráfico

---
+++ copsboot/src/test/java/com/example/copsboot/report/web/CreateReportRequestValidatorTest.java
@@ -0,0 +1,66 @@
+package com.example.copsboot.report.web;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.jetbrains.annotations.NotNull;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import java.time.Instant;
+import java.util.Set;
+import static com.example.copsboot.util.test.ConstraintViolationSetAssert.assertThat;
+
+public class CreateReportRequestValidatorTest {
+    @Test
+    public void givenTrafficIndicentButInvolvedCarsZero_invalid() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            Validator validator = factory.getValidator();
+
+            CreateReportRequest parameters = new CreateReportRequest(Instant.now(),
+                    "The suspect was wearing a black hat",
+                    true,
+                    0,
+                    createImage());
+            Set<ConstraintViolation<CreateReportRequest>> violationSet = validator.validate(parameters);
+            assertThat(violationSet).hasViolationOnPath("");
+        }
+    }
+
+    @Test
+    public void givenTrafficIndicent_involvedCarsMustBePositive() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            Validator validator = factory.getValidator();
+
+            CreateReportRequest parameters = new CreateReportRequest(Instant.now(),
+                    "The suspect was wearing a black hat.",
+                    true,
+                    2,
+                    createImage());
+            Set<ConstraintViolation<CreateReportRequest>> violationSet = validator.validate(parameters);
+            assertThat(violationSet).hasNoViolations();
+        }
+    }
+
+    @Test
+    public void givenNoTrafficIndicent_involvedCarsDoesNotMatter() {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            Validator validator = factory.getValidator();
+
+            CreateReportRequest parameters = new CreateReportRequest(Instant.now(),
+                    "The suspect was wearing a black hat.",
+                    false,
+                    0,
+                    createImage());
+            Set<ConstraintViolation<CreateReportRequest>> violationSet = validator.validate(parameters);
+            assertThat(violationSet).hasNoViolations();
+        }
+    }
+
+    @NotNull
+    private static MockMultipartFile createImage() {
+        return new MockMultipartFile("image", "picture.png", MediaType.IMAGE_PNG_VALUE, new byte[]{1, 2, 3});
+    }
+
+}
