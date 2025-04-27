1e40bd104eba503ac0a14a9cada6f1c4cde6e88e
josephaven <jr457944@gmail.com>
2025-04-26 23:19:31 -0600

    se agregó restricción de rol OFFICER al endpoint POST /api/users y se configuró ruta base en UserRestController

--- copsboot/src/main/java/com/example/copsboot/user/web/UserRestController.java
+++ copsboot/src/main/java/com/example/copsboot/user/web/UserRestController.java
@@ -4,11 +4,12 @@ import com.example.copsboot.user.AuthServerId;
 import com.example.copsboot.user.CreateUserParameters;
 import com.example.copsboot.user.User;
 import com.example.copsboot.user.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -23,7 +24,6 @@ public class UserRestController {
         this.userService = userService;
     }
 
-
     @GetMapping("/me") //<.>
     public Map<String, Object> myself(@AuthenticationPrincipal Jwt jwt) { //<.>
         Optional<User> userByAuthServerId = userService.findUserByAuthServerId(new AuthServerId(UUID.fromString(jwt.getSubject())));
@@ -37,12 +37,12 @@ public class UserRestController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED) //<.>
+    @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('OFFICER')")
     public UserDto createUser(@AuthenticationPrincipal Jwt jwt,
-                              @RequestBody CreateUserRequest request) { //<.>
-        CreateUserParameters parameters = request.toParameters(jwt); //<.>
+                              @Valid @RequestBody CreateUserRequest request) {
+        CreateUserParameters parameters = request.toParameters(jwt);
         User user = userService.createUser(parameters);
-        return UserDto.fromUser(user); //<.>
+        return UserDto.fromUser(user);
     }
-
 }
